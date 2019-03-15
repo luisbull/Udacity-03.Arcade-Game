46dcecee87a80c8fdaa3f7dbdbe6dae1c5919369
Luis <luisbull@hotmail.com>
2019-03-15 17:19:12 +0000

    Big additions to the file.
    There were substantial changes to this file:
    
    - Addition of more enemies(bugs). 
    - Enemies(bugs) have motion now across the game board.
    - Different speeds between the enemies(bugs).
    - Enemies get restart at random positions after  crossing the game board
    - Collision detection code added from -  http://blog.sklambert.com/html5-canvas-game-2d-collision-detection/#d-collision-detection
    - Player goes back to starting position if collides with enemies(bugs). 
    - Alert display if player reaches top of board game.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,59 @@
-// Enemies our player must avoid
-const Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// --------------ENEMIES -------------- //
 
+
+const Enemy = function(x, y, speed) {
+
+    // Here we input all the properties or methods that our new enemy will need
+    // in order to function.
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper we've provided to easily load images.
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
+    this.speed = speed;
+    this.width = 100;
+    this.height = 83;
+    this.collision = false;
 };
 
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Here we create an array that will contain all our new enemies and their
+// positions on the board.
+const allEnemies = [];
+const enemy1 = new Enemy(0, 60, 250);
+const enemy2 = new Enemy(-100, 60, 200);
+const enemy3 = new Enemy(-150, 145, 300);
+const enemy4 = new Enemy(-200, 230, 250);
+const enemy5 = new Enemy(-100, 230, 200);
+allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
+
+
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // Here our enemies get motion and move from left to right of the game board
+  // Here we also should multiply any movement by the dt parameter which will
+  // ensure the game runs at the same speed for all computers.
+  this.x += this.speed * dt;
+
+  // Here we reset our enemies after crossing the game board and give them a random position
+  if (this.x > 500){
+    this.x = -100 * Math.floor(Math.random()* 4) -200;
+  };
+
+// Here we check if enemies and player collide
+// collision code was taken from (see below).  It was modifed to my needs
+// http://blog.sklambert.com/html5-canvas-game-2d-collision-detection/#d-collision-detection
+  if (this.x < player.x + player.width  && this.x + this.width/1.28  > player.x &&
+		this.y < player.y + player.height/1.28 && this.y + this.height > player.y) {
+      this.collision = true;
+
+      // Here we return our player to starting position
+      if(this.collision = true){
+
+        player.x = 200;
+        player.y = 400;
+        console.log('LOST')
+      }
+    }
 };
 
 
@@ -26,17 +63,32 @@ Enemy.prototype.render = function() {
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+// --------------PLAYER -------------- //
+
 const Player = function(x, y, sprite){
   this.sprite = sprite;
   this.x = x;
   this.y = y;
+  this.width = 100;
+  this.height = 83;
 };
 
 
+//Here we create and place the player object in starting position on the game board.
+const player = new Player(200, 400, 'images/char-princess-girl.png');
+
+
+// Here we check if our player reaches the top of the board game
+// and reset its location to starting position.
 Player.prototype.update = function(dt) {
+  if (this.y === -15){
+    this.x = 200;
+    this.y = 400;
+    console.log('WON');
+    alert('Player Won!!');
+
+  }
 };
 
 
@@ -46,20 +98,9 @@ Player.prototype.render = function() {
 };
 
 
-// Here we create an array that contains our new 3 enemies and their positions on the board.
-const allEnemies = [];
-const enemy1 = new Enemy(0, 60);
-const enemy2 = new Enemy(100, 145);
-const enemy3 = new Enemy(200, 230);
-allEnemies.push(enemy1, enemy2, enemy3);
-
-
-//Here we create and place the player object in starting position on the game board.
-const player = new Player(200, 400, 'images/char-boy.png');
-
-
 //Here we allow our player to move and restrict it from going out the game board.
 Player.prototype.handleInput = function(movement){
+
   if (movement === 'left' && this.x > 0){
     this.x -= 100;
 
@@ -67,14 +108,15 @@ Player.prototype.handleInput = function(movement){
     this.x += 100;
 
   }else if(movement === 'up' && this.y > 0) {
-    this.y -= 85;
+    this.y -= 83;
 
   }else if(movement === 'down' && this.y < 340) {
-    this.y += 85;
+    this.y += 83;
+
   };
 };
 
-
+// The following code was given by Udacity.
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
